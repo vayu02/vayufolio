5432208e1b8fd65226c6da1238cc5f46e7ca1853
vayu02 <me@vayubuilds.com>
2023-09-07 08:43:43 +0530

    added link to google doc in portfolio

--- src/portfolio/Portfolio.tsx
+++ src/portfolio/Portfolio.tsx
@@ -1,5 +1,7 @@
 import P1 from '../assets/p1.png'
 import P2 from '../assets/p2.png'
+import P3 from '../assets/P3.png'
+
 import { Link } from 'react-router-dom'
 
 const Portfolio = () => {
@@ -10,6 +12,32 @@ const Portfolio = () => {
           Portfolio
         </h1>
         <div className='grid grid-cols-3 gap-4 text-white'>
+          <div className='relative p-10 overflow-hidden border-2 border-blue-950  bg-gradient-to-br from-transparent via-blue-950 to-transparent rounded-lg'>
+            <img src={P3} alt='-' className='w-[300px] object-cover  h-full' />
+            <div className='absolute inset-0 flex flex-col justify-between p-4 bg-black bg-opacity-50 transition-opacity opacity-0 hover:opacity-100'>
+              <p className='text-[#ccc] font-semibold text-center mb-2'>
+                Google DOC Clone
+              </p>
+              <div className='flex justify-between'>
+                <Link
+                  to={'https://github.com/vayu02/GDOC-Server/'}
+                  rel='noreferrer'
+                  target='_blank'
+                  className='px-4 py-2 bg-blue-500 text-[#ccc] rounded hover:bg-blue-700'
+                >
+                  Github
+                </Link>
+                <Link
+                  to={'https://v-gdoc.netlify.app/'}
+                  rel='noreferrer'
+                  target='_blank'
+                  className='px-4 py-2 bg-blue-500 text-[#ccc] rounded hover:bg-blue-700'
+                >
+                  Preview
+                </Link>
+              </div>
+            </div>
+          </div>
           <div className='relative p-10 overflow-hidden border-2 border-blue-950  bg-gradient-to-br from-transparent via-blue-950 to-transparent rounded-lg'>
             <img src={P1} alt='-' className='w-[300px] object-cover  h-full' />
             <div className='absolute inset-0 flex flex-col justify-between p-4 bg-black bg-opacity-50 transition-opacity opacity-0 hover:opacity-100'>
@@ -36,7 +64,6 @@ const Portfolio = () => {
               </div>
             </div>
           </div>
-
           <div className='relative overflow-hidden p-10 border-2 border-blue-950 bg-gradient-to-br from-transparent via-blue-950 to-transparent rounded-lg'>
             <img src={P2} alt='-' className='object-cover w-[300px] h-full' />
             <div className='absolute inset-0 flex flex-col justify-between p-4 bg-black bg-opacity-50 transition-opacity opacity-0 hover:opacity-100'>
